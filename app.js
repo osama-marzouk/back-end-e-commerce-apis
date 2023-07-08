b2d99530bec6bd5401fc105eaad8656a390baad6
osama-marzouk <osamamarzou8@gmail.com>
2023-07-08 10:21:30 +0300

    create products routes and connect to db

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const app = express();
+const productsRouter = require('./routes/products')
+const mongoose = require('mongoose');
 
-
-
-
-
-
-
-
+const api = process.env.API_URL
 const port = process.env.PORT || 8000
+
+app.use(`${api}/products`, productsRouter)
+
+mongoose.connect(process.env.DATABASE).then(() => console.log('connecting to database')).catch(err => console.log(err))
+
 
 app.listen(port, () => {
     console.log('listening...')
